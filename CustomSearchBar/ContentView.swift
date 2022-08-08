5ba9436746f4d9c38a9136cbef0635ad50fa9ece
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-08-08 11:22:55 -0600

    Initial commit

---
+++ CustomSearchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomSearchBar
+//
+//  Created by User-C on 8/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomSearchBar/CustomSearchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSearchBarApp.swift
+//  CustomSearchBar
+//
+//  Created by User-C on 8/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomSearchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
